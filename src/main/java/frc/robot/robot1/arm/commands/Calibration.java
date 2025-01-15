eb8eee4d13bfa58b80fa3285f2f02cccee905267
Hatulim <tomer10.katz@gmail.com>
2025-01-15 16:54:10 +0200

    create calibration

--- src/main/java/frc/robot/robot1/arm/commands/Calibration.java
+++ src/main/java/frc/robot/robot1/arm/commands/Calibration.java
@@ -5,11 +5,17 @@
 package frc.robot.robot1.arm.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.robot1.arm.constants.ArmConstants.CalibrationConstants;
+import frc.robot.robot1.arm.subsystems.Arm;
 
 public class Calibration extends Command {
+  
+  private Arm arm;
+
   /** Creates a new Calibration. */
-  public Calibration() {
-    // Use addRequirements() here to declare subsystem dependencies.
+  public Calibration(Arm arm) {
+    this.arm = arm;
+    addRequirements(arm);
   }
 
   // Called when the command is initially scheduled.
@@ -18,15 +24,35 @@ public class Calibration extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    if (arm.getArmAngleLimit()) {
+      arm.armAngleMotorSetPower(CalibrationConstants.ARM_ANGLE_POWER);
+    } else {
+      arm.armAngleMotorSetPower(0);
+    }
+
+    if (arm.getIntakeAngleLimit()) {
+      arm.intakeAngleMotorSetPower(CalibrationConstants.INTAKE_ANGLE_POWER);
+    } else {
+      arm.intakeAngleMotorSetPower(0);
+    }
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    arm.stop();
+    
+    arm.armAngleSetPosition(CalibrationConstants.ARM_ANGLE_LIMIT_ANGLE);
+    arm.intakeAngleSetPosition(CalibrationConstants.INTAKE_ANGLE_LIMIT_ANGLE);
+
+    arm.isCalibrated = true;
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return arm.getArmAngleLimit() && arm.getIntakeAngleLimit();
   }
 }
