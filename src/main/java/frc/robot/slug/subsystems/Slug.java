4cb93c16d3e7e8e5dbfa8b25887b642b51d1e800
Ron Shani <ronshan4u@gmail.com>
2025-01-15 15:56:45 +0200

    fixed warnings

--- src/main/java/frc/robot/slug/subsystems/Slug.java
+++ src/main/java/frc/robot/slug/subsystems/Slug.java
@@ -6,10 +6,8 @@ package frc.robot.slug.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.slug.SlugConstants;
 import frc.robot.utils.TalonMotor;
