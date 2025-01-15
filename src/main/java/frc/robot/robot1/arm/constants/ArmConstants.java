02eeb2281f9cf288582ec7fe9382d5b4cabf3afb
Hatulim <tomer10.katz@gmail.com>
2025-01-15 16:50:47 +0200

    rewrite constants

---
+++ src/main/java/frc/robot/robot1/arm/constants/ArmConstants.java
@@ -0,0 +1,108 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.robot1.arm.constants;
+
+import com.ctre.phoenix6.CANBus;
+
+import frc.robot.utils.TalonConfig;
+
+/** Add your docs here. */
+public class ArmConstants {
+
+    public static final double TABLE_HIGHT = 0.845;
+    public static final double ARM1 = 0.535;
+    public static final double ARM2 = 0.1;
+    
+    public static final String NAME = "Arm";
+
+    public static class ArmAngleMotorConstants {
+        public static final int ID = 0;
+        /*TODO: change to canbus at constants when merging the branch */
+        public static final CANBus CAN_BUS = new CANBus();
+        public static final String NAME = "Arm Angle Motor";
+        
+        public static final double KP = 0;
+        public static final double KI = 0;
+        public static final double KD = 0;
+        public static final double KS = 0;
+        public static final double KV = 0;
+        public static final double KA = 0;
+        public static final double KG = 0;
+        
+        public static final double MOTION_MAGIC_VELOCITY = 0;
+        public static final double MOTION_MAGIC_ACCELERATION = 0;
+        public static final double MOTION_MAGIC_JERK = 0;
+        
+        public static final int LIMIT_SWITCH_CHANNEL = 0;
+        
+        public static final boolean IS_BRAKE = true;
+        public static final boolean IS_INVERTED = false;
+        public static final double GEAR_RATIO = 0;
+        
+        public static final TalonConfig CONFIG = new TalonConfig(ID, CAN_BUS, NAME)
+        .withPID(KP, KI, KD, KS, KV, KA, KG)
+        .withMotionMagic(MOTION_MAGIC_VELOCITY, MOTION_MAGIC_ACCELERATION, MOTION_MAGIC_JERK)
+        .withBrake(IS_BRAKE)
+        .withInvert(IS_INVERTED)
+        .withMotorRatio(GEAR_RATIO).withRadiansMotor();
+    }
+
+    public static class IntakeAngleMotorConstants {
+        public static final int ID = 0;
+        /*TODO: change to canbus at constants when merging the branch */
+        public static final CANBus CAN_BUS = new CANBus();
+        public static final String NAME = "Intake Angle Motor";
+        
+        public static final double KP = 0;
+        public static final double KI = 0;
+        public static final double KD = 0;
+        public static final double KS = 0;
+        public static final double KV = 0;
+        public static final double KA = 0;
+        public static final double KG = 0;
+        
+        public static final double MOTION_MAGIC_VELOCITY = 0;
+        public static final double MOTION_MAGIC_ACCELERATION = 0;
+        public static final double MOTION_MAGIC_JERK = 0;
+        
+        public static final int LIMIT_SWITCH_CHANNEL = 0;
+        
+        public static final boolean IS_BRAKE = true;
+        public static final boolean IS_INVERTED = false;
+        public static final double GEAR_RATIO = 0;
+        
+        public static final TalonConfig CONFIG = new TalonConfig(ID, CAN_BUS, NAME)
+        .withPID(KP, KI, KD, KS, KV, KA, KG)
+        .withMotionMagic(MOTION_MAGIC_VELOCITY, MOTION_MAGIC_ACCELERATION, MOTION_MAGIC_JERK)
+        .withBrake(IS_BRAKE)
+        .withInvert(IS_INVERTED)
+        .withMotorRatio(GEAR_RATIO).withRadiansMotor();
+    }
+    
+    public static class CalibrationConstants {
+        public static final double ARM_ANGLE_POWER = -0.1;
+        public static final double INTAKE_ANGLE_POWER = -0.1;
+        
+        public static final double ARM_ANGLE_LIMIT_ANGLE = 0;
+        public static final double INTAKE_ANGLE_LIMIT_ANGLE = 0;
+    }
+
+    public static enum ANGLES {
+        L2(0, 0),
+        L3(0, 0),
+        CORAL_STATION(0, 0),
+        TESTING(0, 0),
+        IDLE(0, 0);
+
+        public final double ARM_ANGLE;
+        public final double INTAKE_ANGLE;
+
+        ANGLES(double armAngle, double intakeAngle) {
+            this.ARM_ANGLE = armAngle;
+            this.INTAKE_ANGLE = intakeAngle;
+        }
+
+    }
+}
