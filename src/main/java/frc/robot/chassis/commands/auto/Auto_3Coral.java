7ad985e25047313e5d2c03daa21b30bfa18e2eb5
nanonoam <97607867+nanonoam@users.noreply.github.com>
2025-01-15 09:08:53 +0200

    forgot radius

--- src/main/java/frc/robot/chassis/commands/auto/Auto_3Coral.java
+++ src/main/java/frc/robot/chassis/commands/auto/Auto_3Coral.java
@@ -12,13 +12,13 @@ import static frc.robot.chassis.commands.auto.AutoUtils.*;
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class Auto_3Coral extends Command {
   Command cmd;
-  pathPoint startingPoint = new pathPoint(5.1, 7.5, Rotation2d.fromDegrees(180), 0.0, false);
-  pathPoint dumy1 = new pathPoint(4.490, 5.825, Rotation2d.fromDegrees(125), 0.0, false);
-  pathPoint dumy2 = new pathPoint(4.490, 5.825, Rotation2d.fromDegrees(150), 0.0, false);
-  pathPoint intakePoint = new pathPoint(1.619, 7.375, Rotation2d.fromDegrees(125), 0.0, false);
-  pathPoint scoring1 = new pathPoint(5.340, 4.853, Rotation2d.fromDegrees(240), 0.0, false);
-  pathPoint scoring2 = new pathPoint(4.773, 5.181, Rotation2d.fromDegrees(240), 0.0, false);
-  pathPoint scoring3 = new pathPoint(4.2, 5.181, Rotation2d.fromDegrees(150), 0.0, false);
+  pathPoint startingPoint = new pathPoint(5.1, 7.5, Rotation2d.fromDegrees(180), 0.5, false);
+  pathPoint dumy1 = new pathPoint(4.490, 5.825, Rotation2d.fromDegrees(125), 0.3, false);
+  pathPoint dumy2 = new pathPoint(4.490, 5.825, Rotation2d.fromDegrees(150), 0.3, false);
+  pathPoint intakePoint = new pathPoint(1.619, 7.375, Rotation2d.fromDegrees(125), 0.5, false);
+  pathPoint scoring1 = new pathPoint(5.340, 4.853, Rotation2d.fromDegrees(240), 0.4, false);
+  pathPoint scoring2 = new pathPoint(4.773, 5.181, Rotation2d.fromDegrees(240), 0.4, false);
+  pathPoint scoring3 = new pathPoint(4.2, 5.181, Rotation2d.fromDegrees(150), 0.4, false);
   
   public Auto_3Coral() {
   }
