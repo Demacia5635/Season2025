5e1831e8f3b186a489229efc2e95de3d03bcb136
Hatulim <tomer10.katz@gmail.com>
2025-01-16 12:44:13 +0200

    format constants

--- src/main/java/frc/robot/robot1/arm/constants/ArmConstants.java
+++ src/main/java/frc/robot/robot1/arm/constants/ArmConstants.java
@@ -21,10 +21,10 @@ public class ArmConstants {
 
     public static class ArmAngleMotorConstants {
         public static final int ID = 0;
-        /*TODO: change to canbus at constants when merging the branch */
+        /* TODO: change to canbus at constants when merging the branch */
         public static final CANBus CAN_BUS = new CANBus();
         public static final String NAME = "Arm Angle Motor";
-        
+
         public static final double KP = 0;
         public static final double KI = 0;
         public static final double KD = 0;
@@ -32,32 +32,34 @@ public class ArmConstants {
         public static final double KV = 0;
         public static final double KA = 0;
         public static final double KG = 0;
-        
+
         public static final double MOTION_MAGIC_VELOCITY = 0;
         public static final double MOTION_MAGIC_ACCELERATION = 0;
         public static final double MOTION_MAGIC_JERK = 0;
-        
+
         public static final int LIMIT_SWITCH_CHANNEL = 0;
-        
+
         public static final boolean IS_BRAKE = true;
         public static final boolean IS_INVERTED = false;
         public static final double GEAR_RATIO = 0;
-        
+
+        /* The angle where the limit switch */
+        public static final double BASE_ANGLE = 0;
+
         public static final TalonConfig CONFIG = new TalonConfig(ID, CAN_BUS, NAME)
-        .withPID(KP, KI, KD, KS, KV, KA, KG)
-        .withMotionMagic(MOTION_MAGIC_VELOCITY, MOTION_MAGIC_ACCELERATION, MOTION_MAGIC_JERK)
-        .withBrake(IS_BRAKE)
-        .withInvert(IS_INVERTED)
-        .withMotorRatio(GEAR_RATIO).withRadiansMotor();
+                .withPID(KP, KI, KD, KS, KV, KA, KG)
                 .withMotionMagic(MOTION_MAGIC_VELOCITY, MOTION_MAGIC_ACCELERATION, MOTION_MAGIC_JERK)
                 .withBrake(IS_BRAKE)
+                .withInvert(IS_INVERTED)
+                .withMotorRatio(GEAR_RATIO).withRadiansMotor();
     }
 
     public static class GripperAngleMotorConstants {
         public static final int ID = 0;
+        /* TODO: change to canbus at constants when merging the branch */
         public static final CANBus CAN_BUS = new CANBus();
-        public static final String NAME = "Intake Angle Motor";
-        
+        public static final String NAME = "Gripper Angle Motor";
+
         public static final double KP = 0;
         public static final double KI = 0;
         public static final double KD = 0;
@@ -65,31 +67,31 @@ public class ArmConstants {
         public static final double KV = 0;
         public static final double KA = 0;
         public static final double KG = 0;
-        
+
         public static final double MOTION_MAGIC_VELOCITY = 0;
         public static final double MOTION_MAGIC_ACCELERATION = 0;
         public static final double MOTION_MAGIC_JERK = 0;
-        
+
         public static final int LIMIT_SWITCH_CHANNEL = 0;
-        
+
         public static final boolean IS_BRAKE = true;
         public static final boolean IS_INVERTED = false;
         public static final double GEAR_RATIO = 0;
-        
+
+        /* The angle in the limit switch */
+        public static final double BASE_ANGLE = 0;
+
         public static final TalonConfig CONFIG = new TalonConfig(ID, CAN_BUS, NAME)
-        .withPID(KP, KI, KD, KS, KV, KA, KG)
-        .withMotionMagic(MOTION_MAGIC_VELOCITY, MOTION_MAGIC_ACCELERATION, MOTION_MAGIC_JERK)
-        .withBrake(IS_BRAKE)
-        .withInvert(IS_INVERTED)
-        .withMotorRatio(GEAR_RATIO).withRadiansMotor();
+                .withPID(KP, KI, KD, KS, KV, KA, KG)
+                .withMotionMagic(MOTION_MAGIC_VELOCITY, MOTION_MAGIC_ACCELERATION, MOTION_MAGIC_JERK)
+                .withBrake(IS_BRAKE)
+                .withInvert(IS_INVERTED)
+                .withMotorRatio(GEAR_RATIO).withRadiansMotor();
     }
-    
+
     public static class CalibrationConstants {
         public static final double ARM_ANGLE_POWER = -0.1;
-        public static final double INTAKE_ANGLE_POWER = -0.1;
-        
-        public static final double ARM_ANGLE_LIMIT_ANGLE = 0;
-        public static final double INTAKE_ANGLE_LIMIT_ANGLE = 0;
+        public static final double GRIPPER_ANGLE_POWER = -0.1;
     }
 
         L2(0, 0),
