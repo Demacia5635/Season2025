56bebb9075436eb2becc3a2310f1396aac3ce7b8
Huston <danielvfx19@gmail.com>
2025-01-27 21:15:42 +0200

    add motor to elevator
    add function to control the motor position voltage power set position and stop

--- src/main/java/frc/robot/robot2/elevator/subsystem/Elevator.java
+++ src/main/java/frc/robot/robot2/elevator/subsystem/Elevator.java
@@ -4,14 +4,55 @@
 
 package frc.robot.robot2.elevator.subsystem;
 
+import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import frc.robot.utils.TalonMotor;
 import frc.robot.robot2.elevator.ElevatorConstants;
+import frc.robot.utils.LogManager;
+
 public class Elevator extends SubsystemBase {
 
+  TalonMotor motor;
   public Elevator() {
     setName(ElevatorConstants.NAME);
 
+    this.motor = new TalonMotor(ElevatorConstants.motorConfig);
+
+    addNT();
+  }
+  private void addNT() {
+
+    SmartDashboard.putData(getName() + "/Motor", motor);
+
+    SmartDashboard.putData(this);
+  }
+
   }
   
+  /*
+   * position in meters
+   */
+  public void setPositionVoltage(double position){
+    if (isCalibrated) {
+      LogManager.log("Elevator has not calibrated", AlertType.kError);
+      return;
+    }
+
+    motor.setPositionVoltage(position);
+  }
+
+  public void setHeight(double height) {
+    motor.setPosition(height);
+  }
+
+  public void setPower(double power) {
+    motor.setDuty(power);
+  }
+
+  public void stop() {
+    motor.stopMotor();
+  }
+
 }
