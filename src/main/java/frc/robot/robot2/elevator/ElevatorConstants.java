9ff43e674fd407454c5591a1835bd7c312b2ef5a
Huston <danielvfx19@gmail.com>
2025-01-27 21:12:35 +0200

    create elevator subsystem
    add constant also

---
+++ src/main/java/frc/robot/robot2/elevator/ElevatorConstants.java
@@ -0,0 +1,12 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.robot2.elevator;
+
+/** Add your docs here. */
+public class ElevatorConstants {
+
+    public static final String NAME = "Elevator";
+
+}
