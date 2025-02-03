2f4c600b5ded241b52a19a9d4c0b6607ff663185
Hatulim <tomer10.katz@gmail.com>
2025-02-03 15:06:58 +0200

    change motor constants to its own class

--- src/main/java/frc/robot/robot2/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/robot2/elevator/ElevatorConstants.java
@@ -14,24 +14,28 @@ public class ElevatorConstants {
 
     public static final String NAME = "Elevator";
 
-    private static int MOTOR_ID = -1;
-    private static CANBus CANBUS = CANBuses.CHASSIS_CAN_BUS;
-    private static double GEAR_RATIO = -1/-1;
-    private static final double kP = -1;
-    private static final double kI = -1;
-    private static final double kD = -1;
-    private static final double kG = -1;
-    private static final double kV = -1;
-    private static final boolean NETURAL_MODE = true;
-    private static final boolean IS_INVERTED = false;
-    private static final double CIRCUMFERENCE = -1;
-    private static final double RAMP_TIME = 0.3;
+    public static class ElevatorMotorConstants {
 
-    public static final TalonConfig motorConfig = 
-    new TalonConfig(MOTOR_ID, CANBUS, "Elevator Motor").withMotorRatio(GEAR_RATIO)
-    .withPID(kP, kI, kD, 0, kV, 0, kG)
-    .withBrake(NETURAL_MODE).withRampTime(RAMP_TIME).withInvert(IS_INVERTED)
-    .withMeterMotor(CIRCUMFERENCE);
+        private static int MOTOR_ID = -1;
+        private static CANBus CANBUS = CANBuses.CHASSIS_CAN_BUS;
+        private static String NAME = "Elevator/Motor";
+        private static double GEAR_RATIO = -1/-1;
+        private static final double kP = -1;
+        private static final double kI = -1;
+        private static final double kD = -1;
+        private static final double kG = -1;
+        private static final double kV = -1;
+        private static final boolean NETURAL_MODE = true;
+        private static final boolean IS_INVERTED = false;
+        private static final double CIRCUMFERENCE = -1;
+        private static final double RAMP_TIME = 0.3;
+        
+        public static final TalonConfig motorConfig = 
+        new TalonConfig(MOTOR_ID, CANBUS, NAME).withMotorRatio(GEAR_RATIO)
+        .withPID(kP, kI, kD, 0, kV, 0, kG)
+        .withBrake(NETURAL_MODE).withRampTime(RAMP_TIME).withInvert(IS_INVERTED)
+        .withMeterMotor(CIRCUMFERENCE);
+    }
 
     public class ElevatorLimits{
         public static final double TOP_LIMIT_POSITION = -1;
