3edecf8313d23a2f08c130eab741222e637ec3a6
Huston <danielvfx19@gmail.com>
2025-02-05 17:58:35 +0200

    I hate to commit

--- src/main/java/frc/robot/Path/Trajectory/FollowTrajectory.java
+++ src/main/java/frc/robot/Path/Trajectory/FollowTrajectory.java
@@ -7,12 +7,9 @@ package frc.robot.Path.Trajectory;
 import java.util.ArrayList;
 
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Path.Utils.PathPoint;
 import frc.robot.chassis.subsystems.Chassis;
-import frc.robot.utils.LogManager;
 
 public class FollowTrajectory extends Command {
   private Chassis chassis;
