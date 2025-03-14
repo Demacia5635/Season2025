864d1762c81a6918dfb2f7b49f5dab6768616857
Huston <danielvfx19@gmail.com>
2025-03-14 18:15:07 +0200

    add climb

---
+++ src/main/java/frc/robot/robot2/climb/command/OpenClimber.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.robot2.climb.command;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.robot2.climb.ClimebConstants;
+import frc.robot.robot2.climb.subsystem.Climb;
+import frc.robot.utils.CommandController;
+import frc.robot.utils.Elastic;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class OpenClimber extends Command {
+
+  private Climb climb;
+
+  public OpenClimber(CommandController controller, Climb climb) {
+    this.climb = climb;
+    addRequirements(climb);
+  }
+
+  @Override
+  public void initialize() {
+    Elastic.selectTab("End Game");
+  }
+
+
+  @Override
+  public void execute() {
+    climb.setClimbPower(ClimebConstants.ClimbConstants.prepareClimbPower);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    climb.stopClimb();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return climb.getLimit();
+  }
+}
