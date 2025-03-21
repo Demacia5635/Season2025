9ad786346b440f53016405c05d2abab7c88ddf8b
Huston <danielvfx19@gmail.com>
2025-03-21 18:36:33 +0200

    change gripper treshold

--- src/main/java/frc/robot/robot1/gripper/constants/GripperConstants.java
+++ src/main/java/frc/robot/robot1/gripper/constants/GripperConstants.java
@@ -28,7 +28,8 @@ public class GripperConstants {
         // public static final int UP_BACK_SENSOR_CHANNEL = 2;
         public static final int DOWN_SENSOR_CHANNEL = 0;
         public static final Pair<Integer, Integer> UP_SENSOR_CHANNELS = new Pair<Integer, Integer>(2,3);
-        public static final double CORAL_IN_SENSOR = 4.2;
+        public static final double CORAL_IN_UP_SENSOR = 0.05;
+        public static final double CORAL_IN_DOWN_SENSOR = 4.2;
     }
 
     /** All the constants for the grab command */
