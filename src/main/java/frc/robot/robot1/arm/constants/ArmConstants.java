27df44cf979f240d8756d8c3fdf88732dce7e6a3
Huston <danielvfx19@gmail.com>
2025-03-22 20:40:49 +0200

    update gripper abs offset

--- src/main/java/frc/robot/robot1/arm/constants/ArmConstants.java
+++ src/main/java/frc/robot/robot1/arm/constants/ArmConstants.java
@@ -131,7 +131,7 @@ public class ArmConstants {
          * back limit -> the minimum angle
          * forward limit -> the maximum angle
          */
-        public static final double ENCODER_BASE_ANGLE = 3.2483987168809145 - 1.5*Math.PI;
+        public static final double ENCODER_BASE_ANGLE = 3.1763315220836814 - 1.5*Math.PI;
         public static final double BACK_LIMIT = 3.7;
         public static final double FWD_LIMIT = 5.4;
 
