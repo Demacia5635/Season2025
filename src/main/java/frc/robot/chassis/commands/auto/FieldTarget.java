8b9419e3ae541c6dcfd9ac22d2edfb10bbb5f627
Huston <danielvfx19@gmail.com>
2025-03-24 23:55:37 +0200

    update l2 offsets of the sadna

--- src/main/java/frc/robot/chassis/commands/auto/FieldTarget.java
+++ src/main/java/frc/robot/chassis/commands/auto/FieldTarget.java
@@ -30,8 +30,8 @@ public class FieldTarget {
     public static final Translation2d realLeftReefOffset = new Translation2d(-0.05,-0.16);
     public static final Translation2d realRightReefOffset = new Translation2d(-0.05,0.16);
 
-    public static Translation2d l2Left = new Translation2d(0.64, -0.12);
-    public static Translation2d l2Right = new Translation2d(0.64, 0.24);
+    public static Translation2d l2Left = new Translation2d(0.67, -0.11);
+    public static Translation2d l2Right = new Translation2d(0.67, 0.26);
     public static Translation2d l3Left = new Translation2d(0.5, -0.12);
     public static Translation2d l3Right = new Translation2d(0.5, 0.22);
 
