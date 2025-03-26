d4ef21df6838e108f49a7bf0282cd3940549817a
Huston <danielvfx19@gmail.com>
2025-03-26 10:49:43 +0200

    DCMP Qual 10

--- src/main/java/frc/robot/robot1/arm/constants/ArmConstants.java
+++ src/main/java/frc/robot/robot1/arm/constants/ArmConstants.java
@@ -189,7 +189,7 @@ public class ArmConstants {
         AFTER_ALGAE_BOTTOM(1.6, 2.5),
         AFTER_ALGAE_TOP(2.5, 4.6),
         CORAL_STATION(1.6, 5.3),
-        CLIMB(2.8 ,5.45),
+        CLIMB(2.766 ,5.4),
         TESTING(0,0),
         STARTING(Math.toRadians(33.7), 3.64),
         IDLE(0,0);
